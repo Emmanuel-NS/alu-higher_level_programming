23a61bf197f5e884d424584958092cdcf7376877
Emmanuel-NS <e.nsabagasa@alustudent.com>
2024-10-25 05:38:25 -0400

    2nd file

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     num_args = len(sys.argv) - 1  # Exclude the script name itself
 
     if num_args == 0:
-        print("0.")
+        print("0 arguments.")  # Updated this line
     else:
         arg_word = "argument" if num_args == 1 else "arguments"
         print(f"{num_args} {arg_word}:")
