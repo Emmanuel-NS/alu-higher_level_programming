363c2c9d967f56c3ee368ca4ee0ae768c7eef10b
Emmanuel-NS <e.nsabagasa@alustudent.com>
2024-10-25 04:46:12 -0400

    11st file

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def add(a, b):
-    return a ^ b
+     result = 1
+    if b < 0:
+        for _ in range(-b):
+            result /= a
+    else:
+        for _ in range(b):
+            result *= a
+    return result
