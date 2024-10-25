d4e3abab91ba5a2c675926206c04a3e122893145
Emmanuel-NS <e.nsabagasa@alustudent.com>
2024-10-25 07:25:25 -0400

    8th file

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
 def multiple_returns(sentence):
     if sentence:  # Check if the sentence is not empty
-        return (len(sentence), sentence[0])  # Return length and first character
+        return (len(sentence), sentence[0])
     else:
         return (0, None)  # Return 0 and None for empty sentence
 
