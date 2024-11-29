80d8d392fc381d2c304b01316d5128444b817ab4
Emmanuel-NS <e.nsabagasa@alustudent.com>
2024-11-29 07:07:25 -0500

    debugging

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # Script that sends GET request for route validation with header parameter
-#curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
+curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
